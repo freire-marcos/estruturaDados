572acbd446d7792faa9488b62e41641fb8d61aeb
freire-marcos <davidglopes00@gmail.com>
2018-12-04 15:02:23 -0300

    keep

--- src/com/marcos/estruturaDados/vetor/teste/Aula02.java
+++ src/com/marcos/estruturaDados/vetor/teste/Aula02.java
@@ -8,6 +8,10 @@ public class Aula02 {
 
 		Vetor vetor = new Vetor(5);
 		
+		vetor.adiciona("puta merda");
+		vetor.adiciona("vai procurar uq fazer man");
+		
+		System.out.println("e aean");
 	}
 
 }
