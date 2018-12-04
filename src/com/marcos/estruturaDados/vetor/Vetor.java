93f4547bbb3cec2c3783e908dd1f990617617662
freire-marcos <davidglopes00@gmail.com>
2018-12-04 15:20:03 -0300

    just

--- src/com/marcos/estruturaDados/vetor/Vetor.java
+++ src/com/marcos/estruturaDados/vetor/Vetor.java
@@ -3,20 +3,34 @@ package com.marcos.estruturaDados.vetor;
 public class Vetor {
 	
 	private String[] elementos;
+	private int tamanho;
 	
 	public Vetor(int capacidade) {
 		
 		this.elementos = new String[capacidade];
 	}
 	
-	public void adiciona(String elemento) {
+//	public void adiciona(String elemento) {
+//		
+//		for(int i = 0; i < this.elementos.length; i++) {
+//			if(this.elementos[i] == null) {
+//				this.elementos[i] = elemento;
+//				break;
+//			}
+//		}
+//	}
+	
+	public void adiciona(String elemento) throws Exception {
 		
-		for(int i = 0; i < this.elementos.length; i++) {
-			if(this.elementos[i] == null) {
-				this.elementos[i] = elemento;
-				break;
-			}
+		if(this.tamanho < this.elementos.length) {
+			
+			this.elementos[this.tamanho] = elemento;
+			this.tamanho++;
 		}
+		else {
+			throw new Exception("Tá vendo que essa porra tá cheia não?");
+		}
+		
 	}
 
 }
--- src/com/marcos/estruturaDados/vetor/teste/Aula02.java
+++ src/com/marcos/estruturaDados/vetor/teste/Aula02.java
@@ -6,10 +6,26 @@ public class Aula02 {
 
 	public static void main(String[] args) {
 
-		Vetor vetor = new Vetor(5);
+		Vetor vetor = new Vetor(2);
 		
-		vetor.adiciona("puta merda");
-		vetor.adiciona("vai procurar uq fazer man");
+		try {
+			vetor.adiciona("puta merda");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			vetor.adiciona("vai procurar uq fazer man");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			vetor.adiciona("vai procurar uq fazer man");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		System.out.println("e aean");
 	}
