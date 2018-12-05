7b15aa2b231a0cb74985856ce01a07467cbc9ee1
marcos <marcosfreire.dev@gmail.com>
2018-12-04 22:30:09 -0300

    testando gitkraken

---
+++ src/com/marcos/estruturaDados/vetor/teste/Aula03.java
@@ -0,0 +1,18 @@
+package com.marcos.estruturaDados.vetor.teste;
+
+import com.marcos.estruturaDados.vetor.Vetor;
+
+public class Aula03 {
+
+	public static void main(String[] args) {
+
+		Vetor vetor = new Vetor(10);
+		
+		vetor.adiciona("A casa do caralho");
+		
+		vetor.adiciona("mermão, pare com isso...");
+		
+		System.out.println("breake");
+	}
+
+}
